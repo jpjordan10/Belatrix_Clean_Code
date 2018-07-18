57090385f2763fdff7ff1a99f5a1d2e7b0feccec
Jean Pierre Jordan Solis <jeanpierrejordan@outlook.com>
2018-07-18 17:32:44 -0500

    OutputParameter

--- CleanCode/04 OutputParameters/OutputParameters.cs
+++ CleanCode/04 OutputParameters/OutputParameters.cs
@@ -1,23 +1,25 @@
 ﻿using CleanCode.Comments;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CleanCode.OutputParameters
 {
     public class OutputParameters
     {
+        int totalCount;
         public void DisplayCustomers()
         {
-            int totalCount = 0;
-            var customers = GetCustomers(1, out totalCount);
+           var customers = GetCustomers(1);
+            
 
             Console.WriteLine("Total customers: " + totalCount);
             foreach (var c in customers)
                 Console.WriteLine(c);
         }
 
-        public IEnumerable<Customer> GetCustomers(int pageIndex, out int totalCount)
-        {
+        public IEnumerable<Customer> GetCustomers(int pageIndex)
+        {             
             totalCount = 100;
             return new List<Customer>();
         }
