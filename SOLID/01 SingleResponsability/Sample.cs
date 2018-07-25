8ffe09125952e13f946c5da9f434286d9908dc36
Jean Pierre Jordan Solis <jeanpierrejordan@outlook.com>
2018-07-25 00:35:26 -0500

    SingleResponsability

--- SOLID/01 SingleResponsability/Sample.cs
+++ SOLID/01 SingleResponsability/Sample.cs
@@ -4,14 +4,23 @@ namespace SOLID._01_SingleResponsability
 {
     public class Sample
     {
-        public void Sum(int a, int b)
+        public void Sum(int firstNumber, int secondNumber)
         {
-            var c = a + b;
-            var m = "";
-            if (c > 0 && c <= 10) m = "Value between 0 and 10";
-            else if (c > 10 && c <= 20) m = "Value between 11 and 20";
-            else if (c > 20 && c <= 30) m = "Value between 11 and 20";
-            Console.WriteLine(string.Format("The sum is: {0} and is in range {1}", c, m));
+            var result = firstNumber + secondNumber;
+            PrintResult(result);
+        }
+
+        private static void PrintResult(int result)
+        {
+            var mesagge = (result > 0 && result <= 10) ? Messages.LessThanEleven : Messages.GreaterThanTen;
+            Console.WriteLine(string.Format("The sum is: {0} and is in range {1}", result, mesagge));
         }
     }
+
+    public static class Messages
+    {
+        public const string LessThanEleven = "Value between 0 and 10";
+        public const string GreaterThanTen = "Value between 11 and 20";
+    }
+
 }
